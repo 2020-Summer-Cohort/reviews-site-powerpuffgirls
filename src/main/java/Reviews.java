4b7f4b9bb804e798fb902f739b211eaa9829b71d
Jason Orabella <jasonorabella@gmail.com>
2020-06-11 14:58:05 -0400

    pogos

--- src/main/java/Reviews.java
+++ src/main/java/Reviews.java
@@ -1,3 +1,29 @@
 public class Reviews {
+    private String showTitle;
+    private Integer showRating;
+    private String airDate;
+    private String description;
 
+    public Reviews(String showTitle, Integer showRating, String airDate, String description) {
+        this.showTitle = showTitle;
+        this.showRating = showRating;
+        this.airDate = airDate;
+        this.description = description;
+    }
+
+    public String getShowTitle() {
+        return showTitle;
+    }
+
+    public Integer getShowRating() {
+        return showRating;
+    }
+
+    public String getAirDate() {
+        return airDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
---
+++ src/main/java/Tags.java
@@ -0,0 +1,14 @@
+import java.util.Collection;
+
+public class Tags {
+    private Collection<Tags> tags;
+
+    public Tags(Collection<Tags> tags) {
+        this.tags = tags;
+    }
+
+    public Collection<Tags> getTags() {
+        return tags;
+    }
+
+}
