2a2795f13c2ecf05f48e97d19d340dc50dcec7df
Riley Adkins <rileyaadkins@gmail.com>
2020-06-15 10:19:34 -0400

    6.15.20

--- src/main/java/org/wecancodeit/reviews/Hashtags.java
+++ src/main/java/org/wecancodeit/reviews/Hashtags.java
@@ -2,12 +2,12 @@ package org.wecancodeit.reviews;
 
 import java.util.Collection;
 
-public class Hashtags extends Reviews {
-    private String hashtagName;
-
-    public Hashtags(String hashtagName, String showTitle, Integer showRating, String airDate, String description) {
-        super(showTitle, showRating, airDate, description);
-        this.hashtagName = hashtagName;
-    }
-
-}
+//public class Hashtags extends Reviews {
+//    private String hashtagName;
+//
+//    public Hashtags(String hashtagName, String showTitle, Integer showRating, String airDate, String description) {
+//        super(showTitle, showRating, airDate, description);
+//        this.hashtagName = hashtagName;
+//    }
+//
+//}
