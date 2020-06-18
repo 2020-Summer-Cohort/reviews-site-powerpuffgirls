4af323b6d38f0bcd48a1726a028f1cb9ecbca1b5
Jason Orabella <jasonorabella@gmail.com>
2020-06-18 13:39:39 -0400

    working build, ben helped

--- src/main/java/org/wecancodeit/reviews/CategoryController.java
+++ src/main/java/org/wecancodeit/reviews/CategoryController.java
@@ -9,30 +9,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CategoryController {
 
-    CategoriesRepository categoriesRepo;
-    ShowsRepository showsRepo;
-    CategoryStorage categoryStorage = new CategoryStorage(categoriesRepo);
-    ShowStorage showStorage = new ShowStorage(showsRepo);
-//    HashtagsStorage hashtagsStorage = new HashtagsStorage();
 
-        public CategoryController(CategoriesRepository categoriesRepo, )
+    CategoryStorage categoryStorage;
+
+        public CategoryController(CategoryStorage categoryStorage){
+
+            this.categoryStorage = categoryStorage;
+        }
         @RequestMapping("categories")
         public String showAllCategories(Model model){
             model.addAttribute("categoryNames", categoryStorage.findAllCategories());
             return "categories-template";
         }
 
-        @RequestMapping("categories/adultcategorynames")
-        public String showAdultAnimationShows(Model model){
-            model.addAttribute("adultcategories", categoryStorage.findCategoriesByName("Adult Animation").getShows());
-            return "adultanimation-template";
+        @RequestMapping("categories/{categoryName}")
+        public String showAdultAnimationShows(Model model, @PathVariable String categoryName){
+            model.addAttribute("adultcategories", categoryStorage.findCategoriesByName(categoryName).getShows());
+            return "adultanimation-template";// category template th: text h1 element
     }
 
-        @RequestMapping("categories/kidscategorynames")
-        public String showKidsCartoonShows(Model model){
-            model.addAttribute("kidscategories", categoryStorage.findCategoriesByName("Kids Cartoons").getShows());
-            return "kidscartoons-template";
-        }
+//        @RequestMapping("categories/kidscategorynames")
+//        public String showKidsCartoonShows(Model model){
+//            model.addAttribute("kidscategories", categoryStorage.findCategoriesByName("Kids Cartoons").getShows());
+//            return "kidscartoons-template";
+//        }
 
 //         @RequestMapping("categories/hashtags")
 //            public String showAllHashtags(Model model){
