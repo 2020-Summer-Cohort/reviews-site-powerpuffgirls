5cdb980a2dcbffe84d98c2e6215b5407167b1ec0
Riley Adkins <rileyaadkins@gmail.com>
2020-06-19 13:38:41 -0400

    hashtags working, need to make endpoint tho

--- src/main/java/org/wecancodeit/reviews/HashtagsRepository.java
+++ src/main/java/org/wecancodeit/reviews/HashtagsRepository.java
@@ -3,5 +3,6 @@ package org.wecancodeit.reviews;
 import org.springframework.data.repository.CrudRepository;
 
 public interface HashtagsRepository extends CrudRepository<Hashtags, Long> {
+    Hashtags findHashtagsByReviews(String hashtagName);
 
 }
