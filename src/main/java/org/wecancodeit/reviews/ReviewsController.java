ccb7b23775e9a0bafaf37708738480f87c72d44b
Jason Orabella <jasonorabella@gmail.com>
2020-06-19 10:43:11 -0400

    500 level error on review page

--- src/main/java/org/wecancodeit/reviews/ReviewsController.java
+++ src/main/java/org/wecancodeit/reviews/ReviewsController.java
@@ -1,5 +1,4 @@
 package org.wecancodeit.reviews;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
