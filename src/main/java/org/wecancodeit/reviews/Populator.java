ee737a3ef49c0714218f9f1c194bc8d3f9090615
Riley Adkins <rileyaadkins@gmail.com>
2020-06-19 11:29:01 -0400

    riley

--- src/main/java/org/wecancodeit/reviews/Populator.java
+++ src/main/java/org/wecancodeit/reviews/Populator.java
@@ -43,6 +43,7 @@ import org.springframework.stereotype.Component;
 
             Reviews review1 = new Reviews(show1, 3, "November 18, 1998", "This was my favorite show growing up. I always wanted to be a powerpuff girl. I loved their tenacity and the show has great villains.",
                     "./powerpuff-girls.png", hashtag1, hashtag3);
+
             Reviews review2 = new Reviews(show2, 5, "November 4, 2013", "Is honestly a great animated series for all ages. It tackles\n" +
                     "            everyday issues with such depth and complexity that children can\n" +
                     "            digest and adults can fully grasp. In terms of characters is one the\n" +
@@ -52,6 +53,7 @@ import org.springframework.stereotype.Component;
                     "            Japanese RPG's and Shojo series.", "./steven-universe.png", hashtag1, hashtag3);
             Reviews review3 = new Reviews(show3, 5, "September 28, 2008", "Good show", "./superjail.jpg",
                     hashtag1);
+
             Reviews review4 = new Reviews(show4, 4, "January 4, 2007", "Afro Samurai focuses on a samurai named Afro whose mission is to get\n" +
                     "            the number 1 headband and avenge his father. The setting is somewhat\n" +
                     "            feudal however some characters have androids and other futuristic\n" +
