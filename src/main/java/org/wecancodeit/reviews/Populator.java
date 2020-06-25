75041af08ed26576b7970704dc6b26a803b521f9
Riley Adkins <rileyaadkins@gmail.com>
2020-06-25 12:48:50 -0400

    CSS stuff

--- src/main/java/org/wecancodeit/reviews/Populator.java
+++ src/main/java/org/wecancodeit/reviews/Populator.java
@@ -55,12 +55,12 @@ public class Populator implements CommandLineRunner {
         hashtagsRepo.save(hashtag2);
         hashtagsRepo.save(hashtag3);
 
-        Reviews review1 = new Reviews("Powerpuff Girls", "3 out of 5", "November 18, 1998",
+        Reviews review1 = new Reviews("Powerpuff Girls", "★★★", "November 18, 1998",
                 "This was my favorite show growing up! I always wanted to be a power puff girl." +
                         " I loved their tenacity and the show has great villains. ",
                 "/images/powerpuffgirlsimage.png", hashtag1, hashtag3);
 
-        Reviews review2 = new Reviews("Steven Universe", "5 out of 5", "November 4, 2013",
+        Reviews review2 = new Reviews("Steven Universe", "★★★★★", "November 4, 2013",
                 "Honestly a great animated series for all ages. It tackles everyday issues with " +
                         "such depth and complexity that children can digest and adults can fully grasp. In terms" +
                         " of characters is one the most diverse shows on television in racial and LGBT representation." +
@@ -68,7 +68,7 @@ public class Populator implements CommandLineRunner {
                         " of the series is the allusion to 90's Japanese RPG's and Shojo series.",
                 "/images/steven-universe.png", hashtag1, hashtag3);
 
-        Reviews review3 = new Reviews("Superjail", "5 out of 5", "September 28, 2008",
+        Reviews review3 = new Reviews("Superjail", "★★★★", "September 28, 2008",
                 "Takes place in another dimension, in an eponymous jail inside of a volcano." +
                         "  Superjail is a psychedelic very violent show that focuses on different" +
                         " aspects of this fictional jail, it mostly operates as a comedy for adults. " +
@@ -76,34 +76,34 @@ public class Populator implements CommandLineRunner {
                 "/images/superjail.jpg",
                 hashtag1, hashtag2);
 
-        Reviews review4 = new Reviews("Afro Samurai", "4 out of 5", "January 4, 2007",
+        Reviews review4 = new Reviews("Afro Samurai", "★★★★", "January 4, 2007",
                 " Afro Samurai focuses on a samurai named Afro whose mission is to" +
                         " get the number 1 headband and avenge his father. The setting is somewhat feudal however some " +
                         "characters have androids and other futuristic technology." +
                         " The show focuses on battles between the eponymous character and others who are seeking the headband",
                 "/images/afro-samurai.png", hashtag2);
 
-        Reviews review5 = new Reviews("Aqua Teen Hunger Force", "4 out of 5", "December 30, 2000",
+        Reviews review5 = new Reviews("Aqua Teen Hunger Force", "★★★★", "December 30, 2000",
                 "Aqua Teen Hunger Force also called ATHF," +
                 " is a comedy that appeared on adult swim. The show featured main characters Meatwad, " +
                 "Mastershake and Frylock as they got into different comedic situations in a fictional universe.", "/images/ATHF.jpg" , hashtag1, hashtag2);
 
-        Reviews review6 = new Reviews("South Park","5 out of 5","August 13, 1997",
+        Reviews review6 = new Reviews("South Park","★★★★★","August 13, 1997",
                 "Truly comedy gold. South Park offers stories based on the current times with a pleasant dark twist." +
                         " I personally love the animation style and the terrible celebrity impersonations.", "/images/SouthPark.jpg", hashtag1, hashtag2 );
 
-        Reviews review7 = new Reviews("Rick and Morty", "2 out of 5", "December 2, 2013",
+        Reviews review7 = new Reviews("Rick and Morty", "★★", "December 2, 2013",
                 "Want to go on a cosmically wacky adventures and visit another dimension?  Rick and Morty is the show for you." +
                         " I truly enjoy the whimsical mind of Rick, the crazy mad scientist, and Morty the shy but spirited kid just " +
                         "looking for fun. Take a peek of what else is out there and take a ride into the universe all while laughing your butt off.",
         "/images/rickANDmorty.jpg", hashtag1);
 
-        Reviews review8 = new Reviews("Spongebob Squarepants", "5 out of 5", "July 17, 1999",
+        Reviews review8 = new Reviews("Spongebob Squarepants", "★★★★", "July 17, 1999",
                 "The peak of children's entertainment, the stories told throughout this series are too relatable. I have been that fool who ripped their pants." +
                         "I’m happy to let my children watch this show. Not only does it have valuable life lessons but it’s also pretty witty!", "/images/spongebob.jpg", hashtag1,
                 hashtag3);
 
-        Reviews review9 = new Reviews("Avatar: The Last Air Bender", "3 out of 5", "Febuary 21, 2005",
+        Reviews review9 = new Reviews("Avatar: The Last Air Bender", "★★★", "Febuary 21, 2005",
                 " This is my favorite show of all time. The storylines are intricate and the character development is unparalleled compared to similar shows.",
                 "/images/ATLAB.jpeg", hashtag1, hashtag3);
 
